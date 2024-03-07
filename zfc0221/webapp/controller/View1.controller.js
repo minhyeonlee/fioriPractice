07c44bdb26d45de5044207677a89f36a7494c4b0
MINHYEON LEE <minhyeonlee1@gmail.com>
2024-03-07 04:26:06 +0000

    Fragment createId

--- zfc0221/webapp/controller/View1.controller.js
+++ zfc0221/webapp/controller/View1.controller.js
@@ -1,10 +1,12 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageBox",
+    "sap/ui/core/Fragment"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, MessageBox,Fragment) {
         "use strict";
 
         return Controller.extend("sync04.zfc0221.controller.View1", {
@@ -12,10 +14,27 @@ sap.ui.define([
 
             },
             onMain: function(){
-                alert("Main View" + this.getView().byId("inpFrag").getValue());
+                let oView = this.getView();
+                let oInput = oView.byId(
+                    Fragment.createId("idFrag", "inpCarrid")
+                );
+                let inpValue = oInput.getValue();
+                alert("Main View " +inpValue);
+
+                //mhlee
+                // let oFragment = Fragment.createId("idFrag", "inpCarrid")
+                // let oFragValue = this.getView().byId(oFragment).getValue();
+                // alert("Main View"+oFragValue);
+
+                //alert( this.getView().byId(
+                    // Fragment.createId("idFrag", "inpCarrid")
+                    // sap.ui.core.Fragment.createId("idFrag", "inpCarrid")
+                //    )
+                //);
+                //alert("Main View " + this.getView().byId("inpCarrid").getValue());
             },
             onFrag: function(){
-                alert("Fragment View" + this.getView().byId("inpCarrid").getValue());
+                alert("Fragment View " + this.getView().byId("inpCarrid").getValue());
             }
         });
     });
