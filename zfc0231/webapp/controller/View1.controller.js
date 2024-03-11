715293064c7cef29ab67e888c7a2d23ddf6aa3a6
MINHYEON LEE <minhyeonlee1@gmail.com>
2024-03-11 06:16:50 +0000

    zfc0230 Default Model Error Example

---
+++ zfc0231/webapp/controller/View1.controller.js
@@ -0,0 +1,33 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller, JSONModel) {
+        "use strict";
+
+        return Controller.extend("sync04.zfc0231.controller.View1", {
+            onInit: function () {
+                let oData = {
+                    skillName: "SAPUI5"
+                };
+                let oModel = new JSONModel( oData );
+                this.getView().setModel( oModel );
+            },
+            onDefault: function() {
+                let oData = {
+                    lastName: "Lee"
+                };
+                let oModel = new JSONModel( oData );
+                this.getView().setModel( oModel );
+            },
+            onGet: function(){
+                let oView = this.getView();
+                let oModel = oView.getModel();
+                let oData = oModel.getData();
+                alert( oData.lastName );
+            }
+        });
+    });
