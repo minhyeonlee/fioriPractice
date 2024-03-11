2cacc37c43dd9b8c167c54b41ff0219ba094f2a1
MINHYEON LEE <minhyeonlee1@gmail.com>
2024-03-11 06:04:35 +0000

    zfc0230 Data Binding

--- zfc0230/webapp/controller/View1.controller.js
+++ zfc0230/webapp/controller/View1.controller.js
@@ -11,19 +11,32 @@ sap.ui.define([
         return Controller.extend("sync04.zfc0230.controller.View1", {
             onInit: function () {
                 // Data Binding
-                let oData = {
-                    lastName: "Lee",
-                    stdNo: 20
-                };
+                // let oData = {
+                //     lastName: "Lee",
+                //     stdNo: 20
+                // };
 
                 //let oModel = new sap.ui.model.json.JSONModel(); // JSON을 담을 공간 생성
                 // oModel.setData( oData );
                 // let oView = this.getView();
                 // oView.setModel( oModel ); // View와 Model 연결
-                let oModel = new JSONModel();
-                oModel.setData( oData );
-                this.getView().setModel( oModel ); //View에 먼저 접근해서 Model을 설정
-                
+                // let oModel = new JSONModel();
+                // oModel.setData( oData );
+                // this.getView().setModel( oModel ); //View에 먼저 접근해서 Model을 설정
+
+            },
+            onDefault: function () {
+                let oData = {
+                    lastName: "Lee"
+                };
+                let oModel = new JSONModel( oData );
+                this.getView().setModel( oModel );
+            },
+            onGet: function () {
+                let oView = this.getView();
+                let oModel = oView.getModel();
+                let oData = oModel.getData();
+                alert( oData.lastName );
             }
         });
     });
